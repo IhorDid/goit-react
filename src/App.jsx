6bccb9d993a558dcd4b7b2876a5eb7826c4e1340
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-05-15 19:40:32 +0300

    add some title

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,249 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const handleClick = (evt) => {
+//     console.log(evt);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={handleClick}>First button</button>
+//       <button onClick={(evt) => console.log(evt)}>Second button</button>
+//     </>
+//   );
+// }
+
+// **************************************************************************
+
+// const CustomButton = ({ message, children }) => {
+//   return <button onClick={() => alert(message)}>{children}</button>;
+// };
+
+// const App = () => {
+//   return (
+//     <>
+//       <CustomButton message="Playing music!">Play some music</CustomButton>
+//       {/* <CustomButton message="Uploading your data!">Upload data</CustomButton> */}
+//     </>
+//   );
+// };
+
+// **************************************************************************
+
+// const App = () => {
+//   let clicks = 0;
+
+//   const handleClick = () => {
+//     clicks = clicks + 1;
+//   };
+
+//   return <button onClick={handleClick}>Current: {clicks}</button>;
+// };
+
+// const App = () => {
+//   console.log(useState());
+// let clicks = 0;
+//   const [clicks, setClicks] = useState(0);
+
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+
+//   return <button onClick={handleClick}>Current: {clicks}</button>;
+// };
+
+// *********************************************************************************
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+//   const handleToggle = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <>
+//       <button onClick={handleClick}>Currenr :{clicks} </button>
+//       <button onClick={handleToggle}>{isOpen ? "Hide" : "Show"}</button>
+//       {isOpen && <p>Now you can see me!</p>}
+//     </>
+//   );
+// };
+
+// **************************************** Паттерн підняття батька
+
+// const ClickCounter = ({ value, onUpdate }) => {
+//   return <button onClick={onUpdate}>Current: {value}</button>;
+// };
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+//   const handleClick = () => setClicks(clicks + 1);
+
+//   return (
+//     <>
+//       <ClickCounter value={clicks} onUpdate={handleClick} />;
+//       <ClickCounter value={clicks} onUpdate={handleClick} />;
+//     </>
+//   );
+// };
+
+// ********************************************************************
+
+// const App = () => {
+//   const [values, setValues] = useState({
+//     x: 0,
+//     y: 0,
+//   });
+
+//   const updateX = () => {
+//     setValues({
+//       ...values,
+//       x: values.x + 1,
+//     });
+//   };
+
+//   const updateY = () => {
+//     setValues({
+//       ...values,
+//       y: values.y + 1,
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <p>
+//         x: {values.x}, y: {values.y}
+//       </p>
+
+//       <button onClick={updateX}>Update x</button>
+//       <button onClick={updateY}>Update y</button>
+//     </div>
+//   );
+// };
+
+// ********************************************************************************************
+// const App = () => {
+// const handleClick = () => console.log(5);
+// return <button onClick={handleClick}>Click</button>;
+
+//   const handleClick = (value) => {
+//     console.log(value);
+//   };
+//   return <button onClick={(value) => console.log(value)}> Click me! </button>;
+// };
+
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+//   const handleClick = () => {
+//     setClicks(clicks + 1);
+//   };
+
+//   useEffect(() => {
+//     document.title = `You clicked ${clicks} times`;
+//   });
+
+//   return (
+//     <>
+//       <button onClick={handleClick}>Click:{clicks}</button>
+//     </>
+//   );
+// };
+
+// ******************************************************************************
+// const Modal = () => {
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       console.log(`Interval - ${Date.now()}`);
+//     }, 2000);
+
+//     return () => {
+//       clearInterval(intervalId);
+//     };
+//   }, []);
+
+//   return <div>Modal</div>;
+// };
+
+// const App = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   return (
+//     <div>
+//       <button onClick={() => setIsOpen(!isOpen)}>
+//         {isOpen ? "Close" : "Open"}
+//       </button>
+//       {isOpen && <Modal />}
+//     </div>
+//   );
+// };
+
+// **********************************************************************************************
+// const App = () => {
+//   const [clicks, setClicks] = useState(0);
+
+// ✅ Залежності вказані правильно
+//   useEffect(() => {
+//     console.log("Clicks updated: ", clicks);
+//   }, [clicks]);
+
+//   return (
+//     <button onClick={() => setClicks(clicks + 1)}>
+//       You clicked {clicks} times
+//     </button>
+//   );
+// };
+
+// ************************************************************************************
+// const App = () => {
+//   const [clicks, setClicks] = useState(() => {
+//     const saveClicks = localStorage.getItem("save") || 0;
+//     return saveClicks;
+//   });
+
+//   useEffect(() => {
+//     localStorage.setItem("save", clicks);
+//   }, [clicks]);
+
+//   return (
+//     <div>
+//       <button onClick={() => setClicks(clicks + 1)}>
+//         You clicked {clicks} times
+//       </button>
+//       <button onClick={() => setClicks(0)}>Reset</button>
+//     </div>
+//   );
+// };
+
+const App = () => {
+  const [clicks, setClicks] = useState(() => {
+    const savedClicks = JSON.parse(localStorage.getItem("saved-clicks"));
+    if (savedClicks !== null) {
+      return savedClicks;
+    }
+    return 0;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-clicks", clicks);
+  }, [clicks]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button onClick={() => setClicks(clicks + 1)}>
+        You clicked {clicks} times
+      </button>
+      <button onClick={() => setClicks(0)}>Reset</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
