44df3dd86c4d2c4201e5e9c58647a12101f64fef
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-05-19 11:21:03 +0300

    function deepEqual

--- src/App.jsx
+++ src/App.jsx
@@ -465,4 +465,37 @@ const App = () => {
   );
 };
 
+function deepEqual(a, b) {
+  if (a === b) {
+    return true;
+  }
+
+  // if (
+  //   typeof a !== "object" ||
+  //   a === null ||
+  //   typeof b !== "object" ||
+  //   b === null
+  // ) {
+  //   return false;
+  // }
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+  for (const key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(
+  deepEqual(
+    { name: "Alice", age: 25, address: { city: "Wonderland" } },
+    { name: "Alice", age: 25, address: { city: "Wonderland" } }
+  )
+);
+
 export default App;
