42498900f6f04e3a8bc8657b40062a73e7be2ba8
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-06-01 16:43:06 +0300

    import searchBox

--- src/Seachbox.jsx
+++ src/Seachbox.jsx
@@ -1,15 +1,15 @@
-const Seachbox = () => {
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    console.log(evt.target.elements.query.value);
-    evt.target.reset();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="query" />
-      <button type="submit">Search</button>
-    </form>
-  );
-};
+// const Seachbox = ({ onSearch }) => {
+//   const handleSubmit = (evt) => {
+//     evt.preventDefault();
+//     onSearch(evt.target.elements.query.value);
+//     evt.target.reset();
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input type="text" name="query" />
+//       <button type="submit">Search</button>
+//     </form>
+//   );
+// };
 
-export default Seachbox;
+// export default Seachbox;
